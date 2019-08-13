3e41ad49a8a9403b1ade51bf4bf319d34bdd5d18
Gireesh Punathil <gpunathi@in.ibm.com>
2019-08-13 13:33:55 +0530

    enable tekton pipelines for openshift #1
    Make the pipelines work for wider deployment targets through:
    1. Eliminate the need for `appsody extract`
    2. Eliminate the need for exposing and usage of docker daemon
    3. Eliminate the need for container mount introspection
    4. Introduce buildah for image extraction and recomposition
    5. Consolidate more pipeline actions in the script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
-FROM gcr.io/cloud-builders/docker
-RUN apt-get update && \
-      apt-get -y install sudo jq wget
-RUN wget https://github.com/appsody/appsody/releases/download/0.4.0/appsody_0.4.0_amd64.deb
-COPY setupAndRunExtract.sh .
-RUN apt install -f ./appsody_0.4.0_amd64.deb
+# Latest buildah is buggy use v1.9.0 until this is resolved:
+# https://github.com/containers/buildah/issues/1821
+FROM quay.io/buildah/stable:v1.9.0
+COPY extract.sh .
+RUN yum -y update && \
+      yum -y install wget
+# Matching appsody binary does not exist in upstream.
+# Provide the proper version once it is available.
+RUN wget https://github.com/appsody/appsody/releases/download/0.X.0/appsody-0.X.0-1.x86_64.rpm
+RUN yum localinstall -y ./appsody-0.X.0-1.x86_64.rpm
+RUN chmod +x extract.sh
---
+++ extractBuildAndPush.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Contract:
+# $context = the extraction root
+# $gitsource = the target application, plus reference to docker image
+# $dockerfile = the target image name
+# Steps:
+# 1. Go to $gitsource. Parse the config yaml to get the source image
+# 2. Pull the image
+# 3. Spin up a container out of it
+# 4. Copy the extracted content from the container mount to $context
+# 5. Copy the git source to $context/user-app
+# 5. Re-build the image, using the Docker file rom the source image
+# 6. Push the image
+# 7. Delete the container, and the image.
+
+stack=`cat $context/../git-source/.appsody-config.yaml | awk -F' ' '{print $2}'`
+buildah pull $stack
+buildah from --name base-extract $stack
+cid=`buildah ps -aq`
+iid=`buildah images -aq`
+mount=`buildah mount base-extract`
+cp -rf $mount/project/* $context
+cp -rf $context/../$gitsource/* $context/user-app
+cd $context
+buildah bud -f Dockerfile -t $dockerimage .
+buildah push $dockerimage
+buildah rm $cid
+buildah rmi $iid
